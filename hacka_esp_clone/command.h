3881069d2cd021943f08949cf359af61ce529b16
BinBin HD <vohoangnguyenphuong@gmail.com>
2019-08-19 17:31:24 +0700

    add clone esp

---
+++ hacka_esp_clone/command.h
@@ -0,0 +1,69 @@
+const char Start = 0x84;
+const char End = 0x95;
+/*--------------------SERVER UPDATE TO CLIENT------------------------------*/
+//char updateMode =1;
+const int typeFixedID = 2;
+const int typemovingSpeed = 3;
+const int typechargingThreshold = 4;
+const int typespinnerSpeed = 5;
+const int typeStop = 6;
+const int typeContinue = 7;
+const int typeControl = 8;
+const int typeMode = 9;
+const int typeIDError = 10;
+const int typeHandshake = 20;
+
+
+/*--------------------UPDATE TO SERVER------------------------------*/
+const int typeupdateID = 29;
+const int typeupdateBattery = 30;
+const int typeupdateMachineStatus = 31;
+const int typeupdatePanel = 32;
+
+/*--------------------UPDATE TO WIFI SLAVE ------------------------------*/
+const char IDCmd = 0x01;
+const char DataCmd = 0x02;
+const char WifiCmd = 0x03;
+const char WifiSSID = 0x0A;
+const char WifiPass = 0x0B;
+const char WifiIP = 0x0C;
+const char WifiPort = 0x0D;
+const char ACKIDCmd = 0x0E;
+
+const char updateCurrentBattery = 0x11;
+const char updateVoltageBattery = 0x12;
+const char updateEnergyBattery = 0x13;
+
+const char updateStatusParameter = 0x16;
+const char updateDirectionParameter = 0x17;
+const char updateLocationParameter = 0x18;
+
+const char updateLocationPanel = 0x20;
+const char updateStatusPanel = 0x21;
+/*--------------------UPDATE WIFI TO MASTER------------------------------*/
+const char AssignedIDCmd = 0x04;
+const char DataRecCmd = 0x05;
+const char TimeoutError = 0x06;
+const char IDError = 0x07;
+
+const char setFixedID = 0x40;
+const char setMovingSpeed = 0x41;
+const char setSpinnerSpeed =0x42;
+const char setChargeThreshold = 0x43;
+const char setMinPower = 0x44;
+const char setMaxPower = 0x45;
+
+const char setMode = 0x50;
+const char setBuildMap = 0x51;
+const char setCleanMode = 0x52;
+const char setMeasureMode = 0x53;
+const char setChargeMode = 0x54;
+const char setStop = 0x55;
+const char setContinue = 0x56;
+
+/*--------------------UPDATE TO CAMERA SLAVE ------------------------------*/
+const char CapImg = 0x08;
+const char StreamCmd = 0x09;
+/*---------------ATTRS------------------------*/
+bool isAllowCheck = true;
+bool isReconnecting = false;
---
+++ hacka_esp_clone/function.ino
@@ -0,0 +1,179 @@
+void CheckWifi() {
+  while (WiFi.status() != WL_CONNECTED) {
+#ifdef DEBUGER
+    Serial.println(".");
+#endif
+    delay(500);
+  }
+}
+void CheckSocket() {
+  if (!isAllowCheck){
+    return;
+  }
+  Serial.print("Check Socket: ");
+  Serial.println(client->connected());
+  if (!client->connected()) {
+   isAllowCheck = false;
+   ReconnectSocket();
+  } else {
+    isReconnecting = false;
+    isAllowCheck = true;
+  }
+}
+void StartCheckSocket() {
+  if (isAllowCheck) {
+    CheckSocket();
+  }
+   else if (!isReconnecting) {
+    ReconnectSocket();
+   }
+}
+void ReconnectSocket() {
+    isReconnecting = true; 
+    client->onData(&handleData, client);
+    client->onConnect(&onConnect, client);
+    client->connect(ip.c_str(), port);
+    isReconnecting = false;
+}
+void Serial_ID() {
+  if (MasterSerial.available())
+  {
+    char inChar = (char)MasterSerial.read();
+    if (inChar == Start) SerialRecv = true;
+    if (inChar == End)
+    {
+      SerialRecv = false;
+      serial_counter = 0;
+      StringComplete = true;
+    }
+    if (SerialRecv)  serial_counter++;
+    if (serial_counter == 2) cmd = inChar;
+    if (serial_counter > 2) InputString += inChar;
+  }
+  if (StringComplete) {
+    switch (cmd) {
+      case IDCmd:
+        ID = InputString;
+        // RegisterClient(client, ID); /// dang ky ID moi
+        Serial.print("ID: ");
+        Serial.println(ID);
+        break;
+      case WifiSSID:
+        ssid = InputString;
+        Serial.print("ssid: ");
+        Serial.println(ssid);
+        break;
+      case WifiPass:
+        password = InputString;
+        Serial.print("password: ");
+        Serial.println(password);
+        break;
+      case WifiIP:
+        ip = InputString;
+        Serial.print("ip: ");
+        Serial.println(ip);
+        break;
+      //      case WifiPort:
+      //        port = atol(InputString.c_str());
+      //        // RegisterClient(client, ID); /// dang ky ID moi
+      //        Serial.print("port: ");
+      //        Serial.println(port);
+      //        break;
+      //      case ACKIDCmd:
+      //        ACK_ID = InputString.toInt();
+      //        Serial.print("ACKID: ");
+      //        Serial.println(ACK_ID);
+      //        break;
+
+
+    }
+    InputString = "";
+    StringComplete = false;
+  }
+}
+void Serial_Wifi() {
+
+  if (MasterSerial.available())
+  {
+    char inChar = (char)MasterSerial.read();
+    if (inChar == Start) SerialRecv = true;
+    if (inChar == End)
+    {
+      SerialRecv = false;
+      serial_counter = 0;
+      StringComplete = true;
+    }
+    if (SerialRecv)  serial_counter++;
+    if (serial_counter == 2) cmd = inChar;
+    if (serial_counter > 2) InputString += inChar;
+  }
+  if (StringComplete) {
+    switch (cmd) {
+      case IDCmd:
+        ID = InputString;
+#ifdef DEBUGER
+        Serial.print("ID: ");
+        Serial.println(ID);
+#endif
+        // RegisterClient(client, ID); /// dang ky ID moi
+        break;
+      case updateCurrentBattery:
+        battery[0] = InputString.toFloat();
+#ifdef DEBUGER
+        Serial.print("Set Current Battery: ");
+        Serial.println(battery[0]);
+#endif
+        break;
+      case updateVoltageBattery:
+        battery[1] = InputString.toFloat();
+#ifdef DEBUGER
+        Serial.print("Set Voltage Battery: ");
+        Serial.println(battery[1]);
+#endif
+        break;
+      case updateEnergyBattery:
+#ifdef DEBUGER
+        Serial.print("Set Energy Battery: ");
+        Serial.println(MaxPower);
+#endif
+        break;
+      case updateStatusParameter:
+#ifdef DEBUGER
+        Serial.print("Set Status Parameter: ");
+        Serial.println(MinPower);
+#endif
+        break;
+      case updateDirectionParameter:
+#ifdef DEBUGER
+        Serial.print("Set Direction Parameter: ");
+        Serial.println(MinPower);
+#endif
+        break;
+      case updateLocationParameter:
+#ifdef DEBUGER
+        Serial.print("Set Location Parameter: ");
+        Serial.println(MinPower);
+#endif
+        break;
+      case updateLocationPanel:
+#ifdef DEBUGER
+        Serial.print("Set Location Panel: ");
+        Serial.println(MinPower);
+#endif
+        break;
+      case updateStatusPanel:
+#ifdef DEBUGER
+        Serial.print("Set Status Panel: ");
+        Serial.println(MinPower);
+#endif
+        break;
+      default:
+        Serial.println("Unknown cmd!!!");
+    }
+    InputString = "";
+    StringComplete = false;
+  }
+}
+void UpdatetoMaster(String Command, String data) {
+  MasterSerial.print(String(Start) + Command + data + String(End));
+}
